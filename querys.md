89db0d5c613551d2f8aa07ef8032ed96e6362edc
jga <javivi02@gmail.com>
2023-08-07 23:46:23 +0200

    Incorporo la ruta de estaciones de trabajo y todo lo correspondiente para que funcione

--- querys.md
+++ querys.md
@@ -70,3 +70,32 @@ LEFT OUTER JOIN Areas ON Prestamos.AreaID = Areas.AreaID
 WHERE (Prestamos.Fecha_devolucion IS NULL)
 ORDER BY Prestamos.PrestamosID DESC`
 
+
+## CONSULTA ESTACIONES DE TRABAJO
+
+`SELECT
+Estaciones_trabajo.Estaciones_trabajoID,
+Estaciones_trabajo.Identificacion,
+Estaciones_trabajo.Direccion_ip,
+Estaciones_trabajo.AreaID,
+Estaciones_trabajo.Caja,
+Estaciones_trabajo.SwitchID,
+Estaciones_trabajo.Puerto_Switch,
+Estaciones_trabajo.Cpu,
+Estaciones_trabajo.Cpu_serie,
+Estaciones_trabajo.Cpu_rtve,
+Estaciones_trabajo.Monitor,
+Estaciones_trabajo.Monitor_serie,
+Estaciones_trabajo.Monitor_rtve,
+Estaciones_trabajo.Mesa_sonidoID,
+Estaciones_trabajo.Mesa_sonido_serie,
+Estaciones_trabajo.Observaciones,
+Edificio.Edificio,
+Planta.Planta,
+Areas.Area,
+Estaciones_trabajo.Desafectado
+FROM Estaciones_trabajo
+LEFT OUTER JOIN Areas ON Estaciones_trabajo.AreaID = Areas.AreaID
+LEFT OUTER JOIN Edificio ON Areas.EdificioID = Edificio.EdificioID
+LEFT OUTER JOIN Planta ON Areas.PlantaID = Planta.PlantaID
+ORDER BY Estaciones_trabajo.Estaciones_trabajoID DESC`
--- src/services/estacionesTrabajoServices.js
+++ src/services/estacionesTrabajoServices.js
@@ -1,10 +1,37 @@
 import { sequelize } from '../bbdd/dbConnection.js'
-import { modeloEstacionesTrabajo} from '../bbdd/models/EstacionesTrabajo.js'
+import { modeloEstacionesTrabajo } from '../bbdd/models/EstacionesTrabajo.js'
 
 const estacionesTrabajo = modeloEstacionesTrabajo(sequelize)
 
 export const getEstacionesTrabajoServices = async () => {
 
-  return await estacionesTrabajo.findAll()
+  const [results, metadata] = await sequelize.query(`SELECT
+    \`Estaciones _trabajo\`.Estaciones_trabajoID,
+    \`Estaciones _trabajo\`.Identificacion,
+    \`Estaciones _trabajo\`.Direccion_ip,
+    \`Estaciones _trabajo\`.AreaID,
+    \`Estaciones _trabajo\`.Caja,
+    \`Estaciones _trabajo\`.SwitchID,
+    \`Estaciones _trabajo\`.Puerto_Switch,
+    \`Estaciones _trabajo\`.Cpu,
+    \`Estaciones _trabajo\`.Cpu_serie,
+    \`Estaciones _trabajo\`.Cpu_rtve,
+    \`Estaciones _trabajo\`.Monitor,
+    \`Estaciones _trabajo\`.Monitor_serie,
+    \`Estaciones _trabajo\`.Monitor_rtve,
+    \`Estaciones _trabajo\`.Mesa_sonidoID,
+    \`Estaciones _trabajo\`.Mesa_sonido_serie,
+    \`Estaciones _trabajo\`.Observaciones,
+    Edificio.Edificio,
+    Planta.Planta,
+    Areas.Area,
+    \`Estaciones _trabajo\`.Desafectado
+    FROM \`Estaciones _trabajo\`
+    LEFT OUTER JOIN Areas ON \`Estaciones _trabajo\`.AreaID = Areas.AreaID
+    LEFT OUTER JOIN Edificio ON Areas.EdificioID = Edificio.EdificioID
+    LEFT OUTER JOIN Planta ON Areas.PlantaID = Planta.PlantaID
+    ORDER BY \`Estaciones _trabajo\`.Estaciones_trabajoID ASC`)
+
+  return results
 
 }
