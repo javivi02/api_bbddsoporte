d1489ae1354b0ef0fa4e6f069cad5d52d5af7160
javivi02@macStudio <javivi02@gmail.com>
2025-09-25 17:40:21 +0200

    Cambios antes del merge

--- src/bbdd/dbConnection.js
+++ src/bbdd/dbConnection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 export const sequelize = new Sequelize('bbdd_soporte', 'root', 'root', {
   host: '192.28.1.2',
-  dialect: 'mysql',
-  port: 3308
+  port: 3308,
+  dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 })
